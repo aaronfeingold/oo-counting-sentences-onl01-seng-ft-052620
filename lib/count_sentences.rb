c7220096ddc0033399c9dce80a6d0a2b43dffde8
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-08 20:08:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,8 @@ class String
   end
 
   def count_sentences
-    sentences = self.gsub(".", "142").gsub("?", "142").gsub("!", "142").split("142").reject { |sentance| sentance == ""}
-    sentences.count 
+    # sentences = self.gsub(".", "142").gsub("?", "142").gsub("!", "142").split("142").reject { |sentance| sentance == ""}
+    # sentences.count 
   end
   
 end
